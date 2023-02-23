49b99b7171009e3ce41e1b3ce94f7125753897b4
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-02-23 11:31:56 +0100

    added toggle functionality

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// phew… not a lot going on here. Please add some code!
+console.clear();
+
+const bookMarkButtons = document.querySelectorAll('.bookmark');
+const answerButtons = document.querySelectorAll('.card__button-answer');
+
+const cardAnswer1 = document.querySelector('[data-js="answer1"]');
+
+
+bookMarkButtons[0].addEventListener("click", () => {
+    bookMarkButtons[0].classList.toggle('bookmark--active');
+});
+
+answerButtons[0].addEventListener("click", () =>{
+    
+    cardAnswer1.classList.contains("card__answer") === true
+    ? answerButtons[0].textContent = "Hide Answer"
+    : answerButtons[0].textContent = "Show Answer";
+
+    cardAnswer1.classList.toggle("card__answer");
+});
